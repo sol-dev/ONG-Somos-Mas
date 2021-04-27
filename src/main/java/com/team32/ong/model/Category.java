d0422c7be6e664a23ee4cc87576b7fc07c0bc03f
Maximiliano-G17 <maxigimenez1792@gmail.com>
2021-04-27 18:37:02 -0300

    adding notnull annotation in the name field

--- src/main/java/com/team32/ong/model/Category.java
+++ src/main/java/com/team32/ong/model/Category.java
@@ -16,6 +16,8 @@ import javax.persistence.TemporalType;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
 
+import com.sun.istack.NotNull;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -26,13 +28,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @SQLDelete(sql = "UPDATE categories SET deleted=true WHERE id = ?")
-@Where(clause = "deleted = true")
+@Where(clause = "deleted = false")
 public class Category {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	@Column(nullable = false)
+	@NotNull
 	private String name;
 	private String description;
 	private String image;
