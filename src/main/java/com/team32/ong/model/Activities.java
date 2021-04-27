fe4f478f78d99e4374f0d2debc06bc6f4d1653f7
AlfonsoRA <ricardo.areiza.01@gmail.com>
2021-04-27 17:44:31 -0300

    Complementacion de la libreria lombok

--- src/main/java/com/team32/ong/model/Activities.java
+++ src/main/java/com/team32/ong/model/Activities.java
@@ -10,18 +10,23 @@ import javax.persistence.TemporalType;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
-@SQLDelete(sql="UPDATE activities SET eliminado = false WHERE id = ?")
-@Where(clause = "eliminado = true")
+@SQLDelete(sql="UPDATE activities SET delete = true WHERE id = ?")
+@Where(clause = "delete = false")
 public class Activities {
 
 	private Long id;
 	private String name;
 	private String content;
 	private String image;
-	private Boolean eliminado;
+	private Boolean delete;
 	
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date create_at;
@@ -29,64 +34,6 @@ public class Activities {
 	@PrePersist
 	private void prePersist() {
 		this.create_at = new Date();
-		this.eliminado = true;
-	}
-
-	public Activities() {
-		super();
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getContent() {
-		return content;
-	}
-
-	public void setContent(String content) {
-		this.content = content;
-	}
-
-	public String getImage() {
-		return image;
-	}
-
-	public void setImage(String image) {
-		this.image = image;
-	}
-
-	public Boolean getEliminado() {
-		return eliminado;
-	}
-
-	public void setEliminado(Boolean eliminado) {
-		this.eliminado = eliminado;
-	}
-
-	public Date getCreate_at() {
-		return create_at;
-	}
-
-	public void setCreate_at(Date create_at) {
-		this.create_at = create_at;
-	}
-
-	@Override
-	public String toString() {
-		return "Activities [id=" + id + ", name=" + name + ", content=" + content + ", image=" + image + ", eliminado="
-				+ eliminado + ", create_at=" + create_at + "]";
+		this.delete = false;
 	}	
 }
