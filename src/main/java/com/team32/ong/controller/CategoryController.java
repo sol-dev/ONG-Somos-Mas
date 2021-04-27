48ff3eeb53be475bddc4a70a3a4112e02e97a8d3
PsyBrainy <martuto93@gmail.com>
2021-04-27 20:01:05 -0300

    added enpoint save in categoryController and added CategoryDTO

--- src/main/java/com/team32/ong/controller/CategoryController.java
+++ src/main/java/com/team32/ong/controller/CategoryController.java
@@ -1,10 +1,20 @@
 package com.team32.ong.controller;
 
+import com.team32.ong.dto.CategoryDTO;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("api/v1/categories")
 public class CategoryController {
 
+
+    @PostMapping("/save")
+    public CategoryDTO save(@Valid CategoryDTO categoryDTO){
+
+        return null;
+    }
 }
---
+++ src/main/java/com/team32/ong/dto/CategoryDTO.java
@@ -0,0 +1,14 @@
+package com.team32.ong.dto;
+
+import lombok.Data;
+
+
+@Data
+public class CategoryDTO {
+
+    private Long id;
+    private String name;
+    private String description;
+    private String image;
+
+}
