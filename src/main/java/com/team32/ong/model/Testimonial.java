476745dc98c971536f1fe0ed00d5e4f08d7e857a
Sanchez Matias <lomatiasleandro@gmail.com>
2021-04-27 15:53:22 -0300

    model Testimonial created

---
+++ src/main/java/com/team32/ong/model/Testimonial.java
@@ -0,0 +1,40 @@
+package com.team32.ong.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table (name = "Testimonials")
+@SQLDelete(sql = "UPDATE categories SET deleted=true WHERE id = ?")
+@Where(clause = "deleted = false")
+public class Testimonial {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="name")
+    private String name;
+
+    @Column(name="image")
+    private String image;
+
+    @Column(name="content")
+    private String content;
+
+    @Column(name="deleted")
+    private Boolean deleted;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name="registDate")
+    private LocalDateTime registDate;
+
+}
