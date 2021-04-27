c82936c095928e35cf1a282d5d1afdd888d2f953
IgOliveto <igoliveto@gmail.com>
2021-04-26 23:22:07 -0300

    Kickoff

---
+++ src/test/java/com/team32/ong/OngApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team32.ong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OngApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
