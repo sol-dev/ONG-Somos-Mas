3aec2767d05d42bdd77bdd0f9ee958dc7e61adf3
PsyBrainy <martuto93@gmail.com>
2021-04-27 21:11:41 -0300

    refactor endpoint save from category whith return http status

--- src/main/java/com/team32/ong/controller/CategoryController.java
+++ src/main/java/com/team32/ong/controller/CategoryController.java
@@ -1,7 +1,10 @@
 package com.team32.ong.controller;
 
 import com.team32.ong.dto.CategoryDTO;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,8 +18,14 @@ public class CategoryController {
 
 
     @PostMapping
-    public ResponseEntity<CategoryDTO> save(@Valid @RequestBody CategoryDTO categoryDTO){
+    public ResponseEntity<CategoryDTO> save(@Valid @RequestBody CategoryDTO categoryDTO, BindingResult result){
 
-        return null;
+        if (result.hasErrors()){
+            return new ResponseEntity(categoryDTO, HttpStatus.NOT_ACCEPTABLE);
+        }else{
+            return new ResponseEntity(categoryDTO, HttpStatus.CREATED);
+        }
+
+
     }
 }
