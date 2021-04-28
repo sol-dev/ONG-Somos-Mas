ef03e74297c38f7e7663bdd3af95a1f1ce53ac51
Sanchez Matias <lomatiasleandro@gmail.com>
2021-04-28 11:32:39 -0300

    endpoint testimonials created

--- src/main/java/com/team32/ong/controller/TestimonialController.java
+++ src/main/java/com/team32/ong/controller/TestimonialController.java
@@ -1,7 +1,24 @@
 package com.team32.ong.controller;
 
+import com.team32.ong.model.Testimonial;
+import com.team32.ong.repository.TestimonialRepository;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 public class TestimonialController {
+
+    private final TestimonialRepository testimonialRepository;
+
+    TestimonialController(TestimonialRepository testimonialRepository) {
+        this.testimonialRepository = testimonialRepository;
+    }
+
+    @PostMapping("/testimonials")
+    Testimonial newEmployee(@Valid @RequestBody Testimonial newTestimonial) {
+        return testimonialRepository.save(newTestimonial);
+    }
 }
