0f78d0b26b07272300b97e751d1c91c156f88926
Sanchez Matias <lomatiasleandro@gmail.com>
2021-04-28 16:41:02 -0300

    saveNewTestimonial method modifed

--- src/main/java/com/team32/ong/controller/TestimonialController.java
+++ src/main/java/com/team32/ong/controller/TestimonialController.java
@@ -1,24 +1,33 @@
 package com.team32.ong.controller;
 
-import com.team32.ong.model.Testimonial;
-import com.team32.ong.repository.TestimonialRepository;
+import com.team32.ong.dto.TestimonialDto;
+import com.team32.ong.service.TestimonialService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.Optional;
 
 @RestController
 public class TestimonialController {
 
-    private final TestimonialRepository testimonialRepository;
-
-    TestimonialController(TestimonialRepository testimonialRepository) {
-        this.testimonialRepository = testimonialRepository;
-    }
+    @Autowired
+    TestimonialService testimonialService;
 
     @PostMapping("/testimonials")
-    Testimonial newEmployee(@Valid @RequestBody Testimonial newTestimonial) {
-        return testimonialRepository.save(newTestimonial);
+    public ResponseEntity<TestimonialDto> saveNewTestimonial(@Valid @RequestBody TestimonialDto newTestimonialDto) {
+        TestimonialDto dtoSaved = null;
+        try{
+            dtoSaved = testimonialService.save(newTestimonialDto);
+            return ResponseEntity.of(Optional.of(dtoSaved));
+        }catch (Exception e){
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
     }
 }
