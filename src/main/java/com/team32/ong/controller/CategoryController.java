2a86985f50b61562ffeccf62bc920d5b652e2768
PsyBrainy <martuto93@gmail.com>
2021-04-28 14:54:56 -0300

    add endpoint controller

--- src/main/java/com/team32/ong/controller/CategoryController.java
+++ src/main/java/com/team32/ong/controller/CategoryController.java
@@ -1,7 +1,8 @@
 package com.team32.ong.controller;
 
 import com.team32.ong.dto.CategoryDTO;
-import org.springframework.http.HttpEntity;
+import com.team32.ong.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -16,14 +17,16 @@ import javax.validation.Valid;
 @RequestMapping("/categories")
 public class CategoryController {
 
+    @Autowired
+    private CategoryService categoryService;
 
     @PostMapping
     public ResponseEntity<CategoryDTO> save(@Valid @RequestBody CategoryDTO categoryDTO, BindingResult result){
 
         if (result.hasErrors()){
-            return new ResponseEntity(categoryDTO, HttpStatus.NOT_ACCEPTABLE);
-        }else{
-            return new ResponseEntity(categoryDTO, HttpStatus.CREATED);
+            return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
+        }else {
+            return new ResponseEntity<>(categoryService.save(categoryDTO), HttpStatus.CREATED);
         }
     }
 }
