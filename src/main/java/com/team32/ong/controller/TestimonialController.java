05287a30ec93a5311a59b0f89d8ddb1243e0e38a
Sanchez Matias <lomatiasleandro@gmail.com>
2021-04-29 11:42:09 -0300

    endpoint route changed

--- src/main/java/com/team32/ong/controller/TestimonialController.java
+++ src/main/java/com/team32/ong/controller/TestimonialController.java
@@ -8,21 +8,20 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.util.Optional;
 
 @RestController
+@RequestMapping("api/v1/testimonials")
 public class TestimonialController {
 
     @Autowired
     TestimonialService testimonialService;
 
-    @PostMapping("/testimonials")
     public ResponseEntity<TestimonialDto> saveNewTestimonial(@Valid @RequestBody TestimonialDto newTestimonialDto,
-
                                                              BindingResult result) {
         if (result.hasErrors()){
             return new ResponseEntity(result.getFieldError().toString(), HttpStatus.BAD_REQUEST);
