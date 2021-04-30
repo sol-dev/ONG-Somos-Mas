154b836627b996cc70fe0b352ec747e25401e044
PsyBrainy <martuto93@gmail.com>
2021-04-30 19:52:25 -0300

    changes date manipulations

--- src/main/java/com/team32/ong/model/OrganizationEntity.java
+++ src/main/java/com/team32/ong/model/OrganizationEntity.java
@@ -4,11 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.UpdateTimestamp;
 import org.hibernate.annotations.Where;
 
 import javax.persistence.*;
 import javax.validation.constraints.Email;
+import java.time.LocalDateTime;
 
 
 @Entity
@@ -16,7 +19,7 @@ import javax.validation.constraints.Email;
 @Data @AllArgsConstructor @NoArgsConstructor @Builder
 @SQLDelete(sql = "UPDATE organization SET deleted=true WHERE id = ?")
 @Where(clause = "deleted = false")
-public class OrganizationEntity extends DateInformation {
+public class OrganizationEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -44,6 +47,14 @@ public class OrganizationEntity extends DateInformation {
 
     @Column(name = "aboutUsText")
     private String aboutUsText;
+
+    @CreationTimestamp
+    @Column(name = "created_date")
+    private LocalDateTime createDate;
+
+    @UpdateTimestamp
+    @Column(name = "last_modified_date")
+    private LocalDateTime modifiedDate;
 
     @Column(name = "deleted")
     private Boolean deleted;
