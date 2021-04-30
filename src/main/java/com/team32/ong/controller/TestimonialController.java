6e6796c5bcb13a27038538061378778d5ef50ddb
Sanchez Matias <lomatiasleandro@gmail.com>
2021-04-30 20:20:21 -0300

    @valid annotation and BindingResult param added

--- src/main/java/com/team32/ong/controller/TestimonialController.java
+++ src/main/java/com/team32/ong/controller/TestimonialController.java
@@ -1,14 +1,17 @@
 package com.team32.ong.controller;
 
 import com.team32.ong.dto.TestimonialDto;
+import com.team32.ong.exception.custom.InvalidDataException;
 import com.team32.ong.service.TestimonialService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import java.util.Optional;
 
 @RestController
@@ -19,7 +22,11 @@ public class TestimonialController {
     TestimonialService testimonialService;
 
     @PostMapping
-    public ResponseEntity<TestimonialDto> createNewTestimonial(@RequestBody TestimonialDto newTestimonialDto) {
+    public ResponseEntity<TestimonialDto> createNewTestimonial(@Valid @RequestBody TestimonialDto newTestimonialDto, BindingResult result) {
+
+        if (result.hasErrors()){
+            throw new InvalidDataException(result);
+        }
 
         TestimonialDto testimonialDtoCreated = testimonialService.save(newTestimonialDto);
 
