8b62d612930b1fbcc48e1986ed456c46852b5342
Sanchez Matias <lomatiasleandro@gmail.com>
2021-04-30 08:34:45 -0300

    imports deleted

--- src/main/java/com/team32/ong/service/impl/TestimonialServiceImpl.java
+++ src/main/java/com/team32/ong/service/impl/TestimonialServiceImpl.java
@@ -2,15 +2,14 @@ package com.team32.ong.service.impl;
 
 import com.team32.ong.dto.TestimonialDto;
 import com.team32.ong.exception.EmptyInputException;
-import com.team32.ong.mapper.TestimonialMapper;
 import com.team32.ong.model.Testimonial;
 import com.team32.ong.repository.TestimonialRepository;
 import com.team32.ong.service.TestimonialService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDateTime;
 
 @Service
 @Transactional
@@ -18,9 +17,6 @@ public class TestimonialServiceImpl implements TestimonialService {
 
     @Autowired
     private TestimonialRepository testimonialRepository;
-
-    @Autowired
-    private TestimonialMapper testimonialMapper;
 
     @Override
     public TestimonialDto save(TestimonialDto testimonialDto) {
@@ -30,12 +26,29 @@ public class TestimonialServiceImpl implements TestimonialService {
             throw new EmptyInputException("601", "Input Fields are empty");
         }
 
-        Testimonial testimonialToSave =testimonialMapper.dtoToEntity(testimonialDto);
+        Testimonial testimonialToSave =this.dtoToEntity(testimonialDto);
+        testimonialToSave.setDeleted(false);
 
         Testimonial testimonial = testimonialRepository.save(testimonialToSave);
 
-        TestimonialDto testimonialDtoSaved = testimonialMapper.modelToDto(testimonial);
+        TestimonialDto testimonialDtoSaved = this.modelToDto(testimonial);
 
         return testimonialDtoSaved;
+    }
+
+    private TestimonialDto modelToDto(Testimonial testimonial){
+
+        ModelMapper mapper = new ModelMapper();
+        TestimonialDto map = mapper.map(testimonial, TestimonialDto.class);
+
+        return map;
+    }
+
+    private Testimonial dtoToEntity(TestimonialDto testimonialDto){
+
+        ModelMapper mapper = new ModelMapper();
+        Testimonial map = mapper.map(testimonialDto, Testimonial.class);
+
+        return map;
     }
 }
