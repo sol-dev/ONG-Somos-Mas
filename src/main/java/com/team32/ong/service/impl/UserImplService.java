f7e4e84ddcbfa84c55f50fbd4e3ac0ca74017b7c
PsyBrainy <martuto93@gmail.com>
2021-05-02 18:07:51 -0300

    added service save user

--- src/main/java/com/team32/ong/service/impl/UserImplService.java
+++ src/main/java/com/team32/ong/service/impl/UserImplService.java
@@ -1,2 +1,35 @@
-package com.team32.ong.service.impl;public class UserImplService {
+package com.team32.ong.service.impl;
+
+import com.team32.ong.dto.UserDto;
+import com.team32.ong.model.User;
+import com.team32.ong.repository.UserRepository;
+import com.team32.ong.service.UserService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserImplService implements UserService {
+
+    @Autowired
+    private UserRepository userRepo;
+
+    @Override
+    public UserDto save(UserDto user) {
+
+        User userEntity = userRepo.save(dtoToEntity(user));
+
+        return entityToDto(userEntity);
+    }
+
+
+    private User dtoToEntity(UserDto userDto){
+        ModelMapper mapper = new ModelMapper();
+        return mapper.map(userDto, User.class);
+    }
+
+    private UserDto entityToDto(User user){
+        ModelMapper mapper = new ModelMapper();
+        return mapper.map(user, UserDto.class);
+    }
 }
