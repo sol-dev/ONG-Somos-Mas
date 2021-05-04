4bc3b978738f2a928d9b933a2e5ef660c5401594
Sanchez Matias <lomatiasleandro@gmail.com>
2021-05-04 16:43:06 -0300

    updateTestimonial method created

--- src/main/java/com/team32/ong/controller/TestimonialController.java
+++ src/main/java/com/team32/ong/controller/TestimonialController.java
@@ -4,12 +4,10 @@ import com.team32.ong.dto.TestimonialDto;
 import com.team32.ong.exception.custom.InvalidDataException;
 import com.team32.ong.service.TestimonialService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.Optional;
@@ -31,5 +29,17 @@ public class TestimonialController {
         TestimonialDto testimonialDtoCreated = testimonialService.save(newTestimonialDto);
 
         return ResponseEntity.of(Optional.of(testimonialDtoCreated));
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<TestimonialDto> updateTestimonial( @PathVariable Long id, @Valid @RequestBody TestimonialDto testimonialDtoToUpdate , BindingResult result){
+
+        if (result.hasErrors()){
+            throw new InvalidDataException(result);
+        }
+
+        TestimonialDto updatedTestimonial = testimonialService.updateById(testimonialDtoToUpdate, id);
+
+        return new ResponseEntity(updatedTestimonial, HttpStatus.OK);
     }
 }
