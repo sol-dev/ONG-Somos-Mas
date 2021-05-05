5376429a679b8f5cfad395d03a4f449a95dd3799
PsyBrainy <martuto93@gmail.com>
2021-05-05 19:13:03 -0300

    a

--- src/test/java/com/team32/ong/OngApplicationTests.java
+++ src/test/java/com/team32/ong/OngApplicationTests.java
@@ -1,9 +1,6 @@
 package com.team32.ong;
 
-import com.team32.ong.model.OrganizationEntity;
-import com.team32.ong.repository.OrganizationRepository;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
