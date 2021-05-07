e7e129bc130f52346657b0aae3f4b04e2f25d171
PsyBrainy <martuto93@gmail.com>
2021-05-07 09:54:54 -0300

    added elements user for jwt

--- src/main/java/com/team32/ong/dto/AuthenticationRequest.java
+++ src/main/java/com/team32/ong/dto/AuthenticationRequest.java
@@ -1,2 +1,9 @@
-package com.team32.ong.dto;public class AuthenticationRequest {
+package com.team32.ong.dto;
+
+import lombok.Data;
+
+@Data
+public class AuthenticationRequest {
+    private String email;
+    private String password;
 }
--- src/main/java/com/team32/ong/dto/AuthenticationResponse.java
+++ src/main/java/com/team32/ong/dto/AuthenticationResponse.java
@@ -1,2 +1,12 @@
-package com.team32.ong.dto;public class AuthenticationResponse {
+package com.team32.ong.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationResponse {
+    private String jwt;
 }
