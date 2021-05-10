d6b8f453b41dcc1bbaa974ccbef873b592e8a2d5
Sol Alvarez <sol.alvarez3421@gmail.com>
2021-05-10 11:58:19 -0300

    new constant and ConstraintViolationException handler added

--- src/main/java/com/team32/ong/constant/ConstantMessage.java
+++ src/main/java/com/team32/ong/constant/ConstantMessage.java
@@ -8,9 +8,10 @@ public class ConstantMessage {
     public static final String MSG_IMAGE_BAD_REQUEST = "Se necesita definir el campo imagen. ";
     public static final String MSG_EMAIL_BAD_REQUEST = "Se necesita definir el campo email. ";
     public static final String MSG_ROL_BAD_REQUEST = "Se necesita definir el campo ROL. ";
-    public static final String MSG_EMAIL_IN_USE = "Este email ya esta registrado";
-    public static final String MSG_EMAIL_NOT_FOUND = "Este mail no es un usuario registrado";
-    public static final String MSG_EMPTY_INPUT = "Debe completar el campo ";
-    public static final String MSG_NOT_FOUND = "No se encontro la entidad con el id ";
-    public static final String MSG_DUPLICATE_KEY = "Ya existe una entidad con esa clave";
+    public static final String MSG_EMAIL_IN_USE = "Este email ya esta registrado. ";
+    public static final String MSG_EMAIL_NOT_FOUND = "Este mail no es un usuario registrado. ";
+    public static final String MSG_EMPTY_INPUT = "Debe completar el campo. ";
+    public static final String MSG_NOT_FOUND = "No se encontro la entidad con el id: ";
+    public static final String MSG_DUPLICATE_KEY = "Ya existe una entidad con esa clave. ";
+    public static final String MSG_EMAIL_INVALID = "Este mail es inválido. ";
 }
