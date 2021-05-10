23a3a8e7155b432295b97111dfb08a3b95ca7a19
PsyBrainy <martuto93@gmail.com>
2021-05-10 10:10:07 -0300

    added menssages

--- src/main/java/com/team32/ong/constants/MessagesConstants.java
+++
@@ -1,8 +0,0 @@
-package com.team32.ong.constants;
-
-public class MessagesConstants {
-
-    public static final String MSG_BAD_REQUEST = "Se necesita definir ";
-    public static final String MSG_EMAIL_IN_USE = "Este email ya esta registrado";
-    public static final String MSG_EMAIL_NOT_FOUND = "Este mail no es un usuario registrado";
-}
--- src/main/java/com/team32/ong/service/impl/UserImplService.java
+++ src/main/java/com/team32/ong/service/impl/UserImplService.java
@@ -1,6 +1,6 @@
 package com.team32.ong.service.impl;
 
-import com.team32.ong.constants.MessagesConstants;
+import com.team32.ong.constant.ConstantMessage;
 import com.team32.ong.dto.UserDTORequest;
 import com.team32.ong.dto.UserDTOResponse;
 import com.team32.ong.exception.custom.BadRequestException;
@@ -39,15 +39,15 @@ public class UserImplService implements UserService, UserDetailsService {
     public UserDTOResponse save(UserDTORequest userDTORequest) throws NotFoundException, BadRequestException {
 
         if (userRepo.existsByEmail(userDTORequest.getEmail())){
-            throw new NotFoundException(MessagesConstants.MSG_EMAIL_IN_USE);
+            throw new NotFoundException(ConstantMessage.MSG_EMAIL_IN_USE);
         }else if (userDTORequest.getEmail() == null){
-            throw new BadRequestException(MessagesConstants.MSG_BAD_REQUEST + "Email");
+            throw new BadRequestException(ConstantMessage.MSG_EMAIL_BAD_REQUEST);
         }else if (userDTORequest.getFirstName() == null){
-            throw new BadRequestException(MessagesConstants.MSG_BAD_REQUEST + "Nombre");
+            throw new BadRequestException(ConstantMessage.MSG_NAME_BAD_REQUEST);
         }else if (userDTORequest.getLastName() == null){
-            throw new BadRequestException(MessagesConstants.MSG_BAD_REQUEST + "Apellido");
+            throw new BadRequestException(ConstantMessage.MSG_LASTNAME_BAD_REQUEST);
         }else if (userDTORequest.getPassword() == null){
-            throw new BadRequestException(MessagesConstants.MSG_BAD_REQUEST + "Contraseña");
+            throw new BadRequestException(ConstantMessage.MSG_PASSWORD_BAD_REQUEST);
         }
         userDTORequest.setPassword(encoder.encode(userDTORequest.getPassword()));
 
@@ -65,7 +65,7 @@ public class UserImplService implements UserService, UserDetailsService {
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
 
         if (!userRepo.existsByEmail(email)){
-            throw new UsernameNotFoundException(MessagesConstants.MSG_EMAIL_NOT_FOUND);
+            throw new UsernameNotFoundException(ConstantMessage.MSG_EMAIL_NOT_FOUND);
         }
         User user = userRepo.findByEmail(email);
 
