a3db5ffbb8365058f2243ea011deb411aa1e522f
caito <caitocd@gmail.com>
2021-05-11 19:51:19 -0300

    fixex conflict constants

--- src/main/java/com/team32/ong/constant/ConstantMessage.java
+++ src/main/java/com/team32/ong/constant/ConstantMessage.java
@@ -13,6 +13,7 @@ public class ConstantMessage {
     public static final String MSG_EMPTY_INPUT = "Debe completar el campo ";
     public static final String MSG_NOT_FOUND = "No se encontro la entidad con el id ";
     public static final String MSG_DUPLICATE_KEY = "Ya existe una entidad con esa clave";
+    public static final String MSG_DELETE_OK = "Se elimino correctamente al usurio con id ";
     public static final String MSG_COMMENT_NOT_FOUND = "No se encontro el comentario con el id ";
     public static final String MSG_EMPTY_COMMENT_BODY = "El cuerpo del comentario no puede etar vacio";
     public static final String MSG_EMPTY_ACTIVITY = "La Actividad No tiene contenido";
