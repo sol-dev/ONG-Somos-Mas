50340dff22cbfdaadf02ae9bcd17d7cc82dc1052
caito <caitocd@gmail.com>
2021-05-11 19:57:53 -0300

    fix conflict constants 4

--- src/main/java/com/team32/ong/constant/ConstantMessage.java
+++ src/main/java/com/team32/ong/constant/ConstantMessage.java
@@ -14,6 +14,4 @@ public class ConstantMessage {
     public static final String MSG_NOT_FOUND = "No se encontro la entidad con el id ";
     public static final String MSG_DUPLICATE_KEY = "Ya existe una entidad con esa clave";
     public static final String MSG_DELETE_OK = "Se elimino correctamente al usurio con id ";
-
-    
 }
