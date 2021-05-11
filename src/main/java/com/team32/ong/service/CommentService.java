b4c24f1058f24ced15ad979a37be67f18723fe79
caito <caitocd@gmail.com>
2021-05-11 14:20:15 -0300

    fixed conflicts

--- src/main/java/com/team32/ong/service/CommentService.java
+++ src/main/java/com/team32/ong/service/CommentService.java
@@ -1,6 +1,6 @@
 package com.team32.ong.service;
 
-<<<<<<< HEAD
+
 import java.util.Optional;
 
 import org.springframework.http.ResponseEntity;
@@ -9,25 +9,16 @@ import org.springframework.stereotype.Service;
 import com.team32.ong.dto.AddCommentBody;
 import com.team32.ong.dto.CommentDto;
 
+
 @Service
 public interface CommentService {
-	
-	CommentDto save(CommentDto commentDto);
-	CommentDto getOne(Long id);
-	CommentDto findById(Long id);
-	ResponseEntity<CommentDto> createNewComment(Long newsId, Long userId, AddCommentBody commentBody);
-	AddCommentBody update(Long id, AddCommentBody commentBody) throws Exception;
-=======
-import com.team32.ong.dto.AddCommentBody;
-import com.team32.ong.dto.CommentDto;
-import com.team32.ong.model.Comment;
 
-import java.util.Optional;
-
-public interface CommentService {
-
+ CommentDto save(CommentDto commentDto);
+ CommentDto getOne(Long id);
+ CommentDto findById(Long id);
+ ResponseEntity<CommentDto> createNewComment(Long newsId, Long userId, AddCommentBody commentBody);
  AddCommentBody update(Long id, AddCommentBody commentDto) throws Exception;
  boolean existsById(Long id) throws Exception;
- Optional<Comment> findById(Long id) throws Exception;
->>>>>>> 69689d3f802e979a07c7560b01995bba9b6d064f
+
+
 }
