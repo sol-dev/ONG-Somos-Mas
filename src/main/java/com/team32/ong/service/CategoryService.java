8fe0c5c53fe0e13b81537a0901794babcb797c50
caito <caitocd@gmail.com>
2021-05-12 13:02:02 -0300

    fixed CategoryService

--- src/main/java/com/team32/ong/service/CategoryService.java
+++ src/main/java/com/team32/ong/service/CategoryService.java
@@ -10,5 +10,5 @@ import java.util.List;
 public interface CategoryService {
 
     CategoryDTO save(CategoryDTO categoryDTO);
-    List<String > viewAll() throws Exception;
+    List<String> viewAll() throws Exception;
 }
