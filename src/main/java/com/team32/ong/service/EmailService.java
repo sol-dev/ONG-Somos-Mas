3a200b233640d1362d1435f5c297c1e208415f33
Sanchez Matias <lomatiasleandro@gmail.com>
2021-05-13 15:13:32 -0300

    sengrid configuration done

--- src/main/java/com/team32/ong/service/EmailService.java
+++ src/main/java/com/team32/ong/service/EmailService.java
@@ -1,8 +1,10 @@
 package com.team32.ong.service;
 
 import com.sendgrid.helpers.mail.Mail;
+import javassist.NotFoundException;
+
+import java.io.IOException;
 
 public interface EmailService {
-    String sendEmail(String email);
-    Mail prepareMail(String email);
+    void sendEmail(String email) throws NotFoundException, IOException;
 }
