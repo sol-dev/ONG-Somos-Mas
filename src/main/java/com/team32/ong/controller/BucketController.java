1bd7fe449cc06c480b027844b2373e26684f73d1
Nuria A. Malet Quintar <nmaletq.pr@gmail.com>
2021-05-14 19:37:57 -0300

    Continued working on exceptions. Added method to check if a file exists in S3

--- src/main/java/com/team32/ong/controller/BucketController.java
+++ src/main/java/com/team32/ong/controller/BucketController.java
@@ -1,18 +1,24 @@
 package com.team32.ong.controller;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import com.amazonaws.services.s3.model.Bucket;
 import com.team32.ong.exception.custom.EmptyInputException;
 import com.team32.ong.service.AmazonClient;
+
+import javassist.NotFoundException;
 
 @RestController
 @RequestMapping("/api/v1/storage/")
@@ -27,9 +33,17 @@ public class BucketController {
     }
     
     @DeleteMapping("/deleteFile")
-    public ResponseEntity<String>  deleteFilePrueba(@RequestParam String fileUrl) {
+    public ResponseEntity<String>  deleteFilePrueba(@RequestParam String fileUrl) throws Throwable{
         return amazonClient.deleteFileFromS3Bucket(fileUrl);
     }
-
     
+    @PostMapping(value="/prueba")
+    public List<Bucket> prueba() throws Throwable{
+        return amazonClient.listBuckets();
+    }
+
+    @GetMapping(value="/prueba2")
+    public Boolean prueba2(@RequestParam String imageUrl) throws Throwable{
+        return amazonClient.imageExists(imageUrl);
+    }
 }
--- src/main/java/com/team32/ong/service/AmazonClient.java
+++ src/main/java/com/team32/ong/service/AmazonClient.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Date;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,10 +17,13 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.Bucket;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.DeleteObjectRequest;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.team32.ong.exception.custom.EmptyInputException;
+
+import javassist.NotFoundException;
 
 
 @Service
@@ -33,9 +38,25 @@ public class AmazonClient {
     @Value("${amazonProperties.secretKey}")
     private String secretKey;
     
-    
-    // pruebas
-    //private String endpointUrl = "https://us-east-1.amazonaws.com";
+    public List<Bucket> listBuckets() throws IOException {
+    	List<Bucket> buckets = null;
+    	try {
+    		BasicAWSCredentials creds = new BasicAWSCredentials(this.accessKey, this.secretKey);
+    	    AmazonS3 s3client = AmazonS3ClientBuilder.standard()
+                    .withRegion("us-east-1")
+                    .withCredentials(new AWSStaticCredentialsProvider(creds))
+                    .build();
+    	    buckets = s3client.listBuckets();
+    	    if(!s3client.doesBucketExistV2(bucketName)){
+    	    	throw new IOException("Problemas con Amazon. Nombre del bucket equivocado");
+    	    } 
+    	} catch (AmazonServiceException e) {
+    		throw new IOException("Problemas con Amazon. AmazonServiceException. accessKey o secretKey son incorrectas");
+        } catch (SdkClientException e) {
+        	throw new IOException("Problemas con Amazon. SdkClientException");
+        }
+    	return buckets;
+    }
 	
 	private File convertMultiPartToFile(MultipartFile file) throws IOException {
 	    File convFile = new File(file.getOriginalFilename());
@@ -49,49 +70,78 @@ public class AmazonClient {
 	    return new Date().getTime() + "-" + multiPart.getOriginalFilename().replace(" ", "_");
 	}
 	
-	private void uploadFileTos3bucket(String fileName, File file) {
-		BasicAWSCredentials creds = new BasicAWSCredentials(this.accessKey, this.secretKey);
-	    AmazonS3 s3client = AmazonS3ClientBuilder.standard()
-                .withRegion("us-east-1")
-                .withCredentials(new AWSStaticCredentialsProvider(creds))
-                .build();
-	    s3client.putObject(new PutObjectRequest(bucketName, fileName, file)
+	private void uploadFileTos3bucket(String fileName, File file) throws IOException {
+		List<Bucket> buckets = listBuckets();
+		if( buckets != null) {
+			BasicAWSCredentials creds = new BasicAWSCredentials(this.accessKey, this.secretKey);
+		    AmazonS3 s3client = AmazonS3ClientBuilder.standard()
+	                .withRegion("us-east-1")
+	                .withCredentials(new AWSStaticCredentialsProvider(creds))
+	                .build();
+		    s3client.putObject(new PutObjectRequest(bucketName, fileName, file)
 	            .withCannedAcl(CannedAccessControlList.PublicRead));
+	    } 
 	}
 	
 	public ResponseEntity<String> uplodFileToS3Bucket(MultipartFile multipartFile) throws EmptyInputException, IOException {
 		if(multipartFile != null && !multipartFile.isEmpty()) {
 			String fileUrl = "";
-			try {
-		        File file = convertMultiPartToFile(multipartFile);
-		        String fileName = generateFileName(multipartFile);
-		        fileUrl = endpointUrl + "/" + bucketName + "/" + fileName;
-		        uploadFileTos3bucket(fileName, file);
-		        file.delete();
-			} catch (IOException ioe) {
-				throw new IOException("No se pudo cargar la imagen: " + multipartFile, ioe);
-			}
+	        File file = convertMultiPartToFile(multipartFile);
+	        String fileName = generateFileName(multipartFile);
+	        fileUrl = endpointUrl + "/" + bucketName + "/" + fileName;
+	        uploadFileTos3bucket(fileName, file);
+	        file.delete();
 		    return new ResponseEntity<>(fileUrl,HttpStatus.OK);
 		} else {
-			throw new EmptyInputException("Tiene que cargar un archivo");
+			throw new EmptyInputException("Tiene que seleccionar un archivo");
 		}    
 	}
 	
-	public ResponseEntity<String> deleteFileFromS3Bucket(String fileUrl) {
+	public ResponseEntity<String> deleteFileFromS3Bucket(String fileUrl) throws Throwable{
 		if(fileUrl.isEmpty()|fileUrl.isBlank()) {
 			throw new EmptyInputException("Tiene que poner la dirección URL del archivo que quiere borrar");
 		}
-	    String fileName = fileUrl.substring(fileUrl.lastIndexOf("/") + 1);
-	    BasicAWSCredentials creds = new BasicAWSCredentials(this.accessKey, this.secretKey);
-	    AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
-                .withRegion("us-east-1")
-                .withCredentials(new AWSStaticCredentialsProvider(creds))
-                .build();
-	    s3Client.deleteObject(new DeleteObjectRequest(bucketName, fileName));
-	    return new ResponseEntity<>("El archivo " + bucketName + "/" + fileName + " se borró correctamente",HttpStatus.OK);
-	}	
-    
+		List<Bucket> buckets = listBuckets();
+		if( buckets == null) {
+			throw new IOException("Problemas con Amazon");
+		} else {
+		    String fileName = fileUrl.substring(fileUrl.lastIndexOf("/") + 1);
+		    BasicAWSCredentials creds = new BasicAWSCredentials(this.accessKey, this.secretKey);
+		    AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
+	                .withRegion("us-east-1")
+	                .withCredentials(new AWSStaticCredentialsProvider(creds))
+	                .build();
+		    if(s3Client.doesObjectExist(bucketName, fileName)) {
+		    	s3Client.deleteObject(bucketName, fileName);
+		    } else {
+		    	throw new NotFoundException("El archivo " + fileName + " no existe");
+		    }
+		    return new ResponseEntity<>("El archivo " + bucketName + "/" + fileName + " se borró correctamente",HttpStatus.OK);
+		}
+	}
+	
+	public Boolean imageExists(String imageUrl) throws Throwable {
+		if(imageUrl.isEmpty()|imageUrl.isBlank()) {
+			throw new EmptyInputException("Tiene que poner la dirección URL de la imagen");
+		}
+		List<Bucket> buckets = listBuckets();
+		if( buckets != null) {
+		    String fileName = imageUrl.substring(imageUrl.lastIndexOf("/") + 1);
+		    BasicAWSCredentials creds = new BasicAWSCredentials(this.accessKey, this.secretKey);
+		    AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
+	                .withRegion("us-east-1")
+	                .withCredentials(new AWSStaticCredentialsProvider(creds))
+	                .build();
+		    if(s3Client.doesObjectExist(bucketName, fileName)) {
+		    	return true;
+		    } 
+		}
+		return false;
+	}
+		
 }
+    
+
 
 	
 
