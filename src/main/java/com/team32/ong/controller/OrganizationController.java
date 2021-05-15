d86f05562bcde655c4ba8c2ed50e837ff9286a08
Sol Alvarez <sol.alvarez3421@gmail.com>
2021-05-15 20:11:51 -0300

    fix merge

--- src/main/java/com/team32/ong/controller/OrganizationController.java
+++ src/main/java/com/team32/ong/controller/OrganizationController.java
@@ -1,63 +1,10 @@
 package com.team32.ong.controller;
 
-import com.team32.ong.dto.OrganizationDTO;
-import com.team32.ong.exception.custom.InvalidDataException;
-import com.team32.ong.service.IOrganizationService;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import javax.validation.Valid;
-
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-
-
 @RestController
-@RequestMapping("api/v1/organization")
+@RequestMapping("/api/v1/organization")
 public class OrganizationController {
-    
-    @Autowired
-    @Qualifier("organizationService")
-    private IOrganizationService organizationService;
-
-    @GetMapping(value = "/public")
-    public List<OrganizationDTO> getPublicInfo(){
-        return organizationService.findActives();
-    }
-
-    @PostMapping(value = "/new", consumes = "application/json")
-    public ResponseEntity<OrganizationDTO> newOrganization(@Valid @RequestBody OrganizationDTO organization, BindingResult result){
-        Map<String, Object> response = new HashMap<>();
-        OrganizationDTO newDto = null;
-        newDto = organizationService.save(organization);
-        response.put("message", "Organizacion Guardada con exito!");
-        response.put("organization", newDto);
-        return new ResponseEntity(response, HttpStatus.CREATED);
-    }
-
-    @GetMapping(value = "/id")
-    public ResponseEntity<OrganizationDTO> findById( @RequestParam("id") Long id){
-       return new ResponseEntity<OrganizationDTO>(organizationService.findById(id), HttpStatus.FOUND);
-    }
- 
-    @GetMapping(value = "/all")
-    public List<OrganizationDTO> findAll(){
-        return organizationService.findAll();
-    }
-
-    @PostMapping(value="/delete")
-    public void softDelete(@RequestParam("id") Long id) {
-        organizationService.softDelete(id);
-    }
-
 
 
 }
--- src/main/java/com/team32/ong/model/OrganizationEntity.java
+++ src/main/java/com/team32/ong/model/OrganizationEntity.java
@@ -6,10 +6,9 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.UpdateTimestamp;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.DynamicUpdate;
-import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.annotations.Where;
 
 import javax.persistence.*;
 import javax.validation.constraints.Email;
@@ -22,6 +21,8 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@SQLDelete(sql = "UPDATE organization SET deleted=true WHERE id = ?")
+@Where(clause = "deleted = false")
 public class OrganizationEntity {
 
     @Id
@@ -62,7 +63,7 @@ public class OrganizationEntity {
     @Column(name = "last_modified_date")
     private LocalDateTime modifiedDate;
 
-    @Column(name = "deleted", columnDefinition = "boolean default false")
+    @Column(name = "deleted")
     private Boolean deleted;
 
 
