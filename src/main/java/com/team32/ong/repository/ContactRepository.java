bc51b2c685967ecbcc5dd7d375f48bb4693cf5fc
Sanchez Matias <lomatiasleandro@gmail.com>
2021-05-18 16:25:56 -0300

    contact repository created

---
+++ src/main/java/com/team32/ong/repository/ContactRepository.java
@@ -0,0 +1,9 @@
+package com.team32.ong.repository;
+
+import com.team32.ong.model.Contact;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ContactRepository extends JpaRepository<Contact, Long> {
+}
