b6d75494fa9dff1aef337d00df2b9650a6c4a686
PsyBrainy <martuto93@gmail.com>
2021-05-19 09:37:54 -0300

    added contact controller create

--- src/main/java/com/team32/ong/controller/ContactController.java
+++ src/main/java/com/team32/ong/controller/ContactController.java
@@ -1,9 +1,23 @@
 package com.team32.ong.controller;
 
+import com.team32.ong.dto.ContactDTO;
+import com.team32.ong.service.ContactService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/v1")
+@RequestMapping("api/v1/contact")
 public class ContactController {
+
+    @Autowired
+    private ContactService contactService;
+
+    @PostMapping
+    public ResponseEntity<ContactDTO> create(ContactDTO contactDTO){
+        return new ResponseEntity<>(contactService.save(contactDTO), HttpStatus.CREATED);
+    }
 }
