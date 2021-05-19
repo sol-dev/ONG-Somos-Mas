5cd5684e66d231ed03d688d2a254e3aa03f19ec1
PsyBrainy <martuto93@gmail.com>
2021-05-19 09:32:32 -0300

    added service in the interface

--- src/main/java/com/team32/ong/service/ContactService.java
+++ src/main/java/com/team32/ong/service/ContactService.java
@@ -1,4 +1,10 @@
 package com.team32.ong.service;
 
+import com.team32.ong.dto.ContactDTO;
+import org.springframework.stereotype.Service;
+
+@Service
 public interface ContactService {
+
+    ContactDTO save(ContactDTO contactDTO);
 }
