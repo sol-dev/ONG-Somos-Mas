444d97b02d63da6df9e49ad33618989660692280
PsyBrainy <martuto93@gmail.com>
2021-05-21 18:55:02 -0300

    added controller save in slide

--- src/main/java/com/team32/ong/controller/SlidesController.java
+++ src/main/java/com/team32/ong/controller/SlidesController.java
@@ -1,9 +1,11 @@
 package com.team32.ong.controller;
 
+import com.team32.ong.dto.SlideDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
@@ -19,6 +21,11 @@ public class SlidesController {
 	@GetMapping(value = "/list")
 	public ResponseEntity<Map<Integer, String>> getImageAndOrderList() {
 		return new ResponseEntity<>(slideService.imageAndOrder(), HttpStatus.OK);
+	}
+
+	@PostMapping
+	public ResponseEntity<SlideDto> save(SlideDto slideDto){
+		return new ResponseEntity<>(slideService.save(slideDto), HttpStatus.CREATED);
 	}
 
 }
