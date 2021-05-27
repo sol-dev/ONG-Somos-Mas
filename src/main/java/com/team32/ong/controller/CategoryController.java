b39f3af4e646696858ec1467aad5bab2a96501eb
Maximiliano-G17 <maxigimenez1792@gmail.com>
2021-05-27 12:43:33 -0300

    adding documentation to category

--- src/main/java/com/team32/ong/controller/CategoryController.java
+++ src/main/java/com/team32/ong/controller/CategoryController.java
@@ -1,35 +1,33 @@
 package com.team32.ong.controller;
 
+import java.util.List;
 
-import com.team32.ong.dto.CategoryDTO;
-import com.team32.ong.dto.ModifyCategoryDTO;
-import com.team32.ong.exception.custom.BadRequestException;
-import com.team32.ong.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.*;
+
+import com.team32.ong.dto.CategoryDTO;
 import com.team32.ong.dto.ListCategoryNameDTO;
+import com.team32.ong.dto.ModifyCategoryDTO;
+import com.team32.ong.exception.ErrorResponse;
+import com.team32.ong.exception.custom.BadRequestException;
+import com.team32.ong.service.CategoryService;
 
-
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import javassist.NotFoundException;
 
-import javax.validation.Valid;
-import org.springframework.web.bind.annotation.*;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.List;
-
+@ApiOperation(value="/api/v1/categories", tags="Category Profile Controller")
 @RestController
 @RequestMapping("api/v1/categories")
 public class CategoryController {
@@ -37,25 +35,53 @@ public class CategoryController {
     @Autowired
     private CategoryService categoryService;
 
+    @ApiOperation(value="Insert a Category", response=CategoryDTO.class)
+    @ApiResponses(value = {
+    		@ApiResponse(code = 201, message = "CREATED", response = CategoryDTO.class),
+    		@ApiResponse(code = 401, message = "UNAUTHORIZED!", response = ErrorResponse.class),
+    		@ApiResponse(code = 403, message = "FORBIDDEN!", response = ErrorResponse.class),
+    		@ApiResponse(code = 404, message = "NOT FOUND", response = ErrorResponse.class)
+    })
     @PostMapping
     public ResponseEntity<CategoryDTO> save(@RequestBody CategoryDTO categoryDTO) throws BadRequestException {
 
         return new ResponseEntity<>(categoryService.save(categoryDTO), HttpStatus.CREATED);
           
     }
-
+    
     //only for admin
+    @ApiOperation(value="Fetch Category by Id", response=CategoryDTO.class)
+    @ApiResponses(value = {
+    		@ApiResponse(code = 200, message = "SUCCESS", response = CategoryDTO.class),
+    		@ApiResponse(code = 401, message = "UNAUTHORIZED!", response = ErrorResponse.class),
+    		@ApiResponse(code = 403, message = "FORBIDDEN!", response = ErrorResponse.class),
+    		@ApiResponse(code = 404, message = "NOT FOUND", response = ErrorResponse.class)
+    })
     @GetMapping(value = "/id")
     public ResponseEntity<CategoryDTO> findById(@RequestParam("id") Long id) throws NotFoundException{
         return new ResponseEntity<CategoryDTO>(categoryService.findById(id), HttpStatus.OK);
     }
-    
+       
+    @ApiOperation(value="Soft delete a Category by Id", response=CategoryDTO.class)
+    @ApiResponses(value = {
+    		@ApiResponse(code = 200, message = "SUCCESS", response = CategoryDTO.class),
+    		@ApiResponse(code = 401, message = "UNAUTHORIZED!", response = ErrorResponse.class),
+    		@ApiResponse(code = 403, message = "FORBIDDEN!", response = ErrorResponse.class),
+    		@ApiResponse(code = 404, message = "NOT FOUND", response = ErrorResponse.class)
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<?> deleteCategory(@PathVariable Long id) throws NotFoundException{
     	categoryService.delete(id);
     	return new ResponseEntity<>(HttpStatus.OK);
     }
 
+    @ApiOperation(value="Update the Details of a Category", response=CategoryDTO.class)
+    @ApiResponses(value = {
+    		@ApiResponse(code = 200, message = "SUCCESS", response = CategoryDTO.class),
+    		@ApiResponse(code = 401, message = "UNAUTHORIZED!", response = ErrorResponse.class),
+    		@ApiResponse(code = 403, message = "FORBIDDEN!", response = ErrorResponse.class),
+    		@ApiResponse(code = 404, message = "NOT FOUND", response = ErrorResponse.class)
+    })
     @PutMapping("/{id}")
     public ResponseEntity<?> update(@PathVariable("id") Long id,@RequestBody ModifyCategoryDTO categoryDTO)
             throws Exception {
@@ -67,6 +93,13 @@ public class CategoryController {
         return new ResponseEntity<>(updatedCategory, HttpStatus.OK);
     }
 
+    @ApiOperation(value="Fetch all Category", response=Iterable.class)
+    @ApiResponses(value = {
+    		@ApiResponse(code = 200, message = "SUCCESS", response = Iterable.class),
+    		@ApiResponse(code = 401, message = "UNAUTHORIZED!", response = ErrorResponse.class),
+    		@ApiResponse(code = 403, message = "FORBIDDEN!", response = ErrorResponse.class),
+    		@ApiResponse(code = 404, message = "NOT FOUND", response = ErrorResponse.class)
+    })
     @GetMapping
     public ResponseEntity<?> listCategories() throws Exception {
 
--- src/main/java/com/team32/ong/dto/CategoryDTO.java
+++ src/main/java/com/team32/ong/dto/CategoryDTO.java
@@ -1,5 +1,7 @@
 package com.team32.ong.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,11 +13,16 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@ApiModel("Categoty Model")
 public class CategoryDTO {
 
+	@ApiModelProperty(notes="Id of the Category", name="id", value="1")
     private Long id;
+	@ApiModelProperty(notes="Name of the Category", name="name", required=true, value="Image")
     private String name;
+	@ApiModelProperty(notes="Description of the Category", name="description", value="Here would go the description")
     private String description;
+	@ApiModelProperty(notes="Image of the Category", name="image", value="Here would go the image")
     private String image;
 
 }
