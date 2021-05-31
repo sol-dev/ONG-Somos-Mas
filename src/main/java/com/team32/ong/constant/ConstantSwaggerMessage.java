73e67ed9a7d8a0c70260a29f0f7886c764e183b3
Sanchez Matias <lomatiasleandro@gmail.com>
2021-05-31 13:11:18 -0300

    constantSwaggerMessage class created

---
+++ src/main/java/com/team32/ong/constant/ConstantSwaggerMessage.java
@@ -0,0 +1,14 @@
+package com.team32.ong.constant;
+
+public class ConstantSwaggerMessage {
+    public static final String MSG_TESTIMONIALDTO_NAME_VALUE = "The testimonial's name";
+    public static final String MSG_TESTIMONIALDTO_IMAGE_VALUE = "The testimonial's image";
+    public static final String MSG_TESTIMONIALDTO_CONTENT_VALUE = "The testimonial's content";
+    public static final String MSG_TESTIMONIAL_CONTROLLER_API_VALUE = "Set of endpoints for Creating, Updating and Deleting of Testimonial.";
+    public static final String MSG_TESTIMONIAL_CONTROLLER_API_TAGS = "Testimonial controller";
+    public static final String MSG_TESTIMONIAL_CONTROLLER_APIOPERATION_CREATE_VALUE= "Create a testimonial";
+    public static final String MSG_TESTIMONIAL_CONTROLLER_APIOPERATION_UPDATE_VALUE= "Update a testimonial";
+    public static final String MSG_TESTIMONIAL_CONTROLLER_APIOPERATION_UPDATE_NOTE= "Update a testimonial by Id";
+    public static final String MSG_TESTIMONIAL_CONTROLLER_APIOPERATION_DELETE_VALUE= "Delete a testimonial";
+    public static final String MSG_TESTIMONIAL_CONTROLLER_APIOPERATION_DELETE_NOTE= "Delete a testimonial by Id";
+}
